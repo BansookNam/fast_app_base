8ce94d3922b7e6491537fab6766230a059c2d809
Bansook Nam <bs.nam@lawfully.com>
2023-03-18 09:55:39 +0900

    dialog 샘플 코드 수정.

--- lib/screen/dialog/d_message.dart
+++ lib/screen/dialog/d_message.dart
@@ -32,7 +32,6 @@ class _MessageDialogState extends DialogState<MessageDialog> {
 
   @override
   Widget build(BuildContext context) {
-    const radius = Radius.circular(14);
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Column(
@@ -40,57 +39,56 @@ class _MessageDialogState extends DialogState<MessageDialog> {
           Expanded(
             child: Container(),
           ),
-          Container(
-              constraints: const BoxConstraints(maxHeight: 500),
-              decoration: BoxDecoration(
-                  color: context.appColors.drawerBg,
-                  borderRadius: const BorderRadius.only(topLeft: radius, topRight: radius)),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Text(
-                        widget.message!,
-                        style: TextStyle(
-                            fontSize: widget.fontSize, height: 1.8, color: context.appColors.text),
-                        textAlign: widget.textAlign,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  )
-                ],
-              )),
-          Line(color: context.appColors.divider),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Tap(
-                  onTap: () {
-                    widget.hide(SimpleResult.success());
-                  },
-                  child: Container(
-                      alignment: Alignment.center,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          color: context.appColors.drawerBg,
-                          borderRadius:
-                              const BorderRadius.only(bottomLeft: radius, bottomRight: radius)),
-                      child: Text(
-                        widget.buttonText ?? 'close'.tr(),
-                        style: TextStyle(
-                          color: context.appColors.confirmText,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                          fontStyle: FontStyle.normal,
+          Material(
+            color: Colors.transparent,
+            child: Container(
+                constraints: BoxConstraints(maxHeight: context.deviceHeight),
+                decoration: BoxDecoration(
+                    color: context.appColors.drawerBg, borderRadius: BorderRadius.circular(15)),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Text(
+                              widget.message!,
+                              style: TextStyle(
+                                  fontSize: widget.fontSize,
+                                  height: 1.8,
+                                  color: context.appColors.text),
+                              textAlign: widget.textAlign,
+                            ),
+                          ),
                         ),
-                      )),
-                ),
-              )
-            ],
+                        const SizedBox(
+                          height: 4,
+                        )
+                      ],
+                    ),
+                    Line(color: context.appColors.divider),
+                    Tap(
+                      onTap: () {
+                        widget.hide(SimpleResult.success());
+                      },
+                      child: Container(
+                          height: 50,
+                          alignment: Alignment.center,
+                          child: Text(
+                            widget.buttonText ?? 'close'.tr(),
+                            style: TextStyle(
+                              color: context.appColors.confirmText,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          )),
+                    )
+                  ],
+                )),
           ),
           Expanded(
             child: Container(),
