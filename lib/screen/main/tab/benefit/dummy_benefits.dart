4afe37efe13306710c4b3ef84b4e7b014c2e1bda
Bansook Nam <bs.nam@lawfully.com>
2023-03-18 19:08:43 +0900

    혜택 메뉴 추가

---
+++ lib/screen/main/tab/benefit/dummy_benefits.dart
@@ -0,0 +1,11 @@
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/screen/main/tab/benefit/vo_benefit_menu.dart';
+
+final benefitMenus = <BenefitMenu>[
+  BenefitMenu('$basePath/benefit/benefit_01.png', '포인트 받기', '친구와 함께 토스 켜고'),
+  BenefitMenu('$basePath/benefit/benefit_02.png', '얼마 받을지 보기', '이번 주 미션하면'),
+  BenefitMenu('$basePath/benefit/benefit_03.png', '포인트 받기', '오늘의 행운복권'),
+  BenefitMenu('$basePath/benefit/benefit_04.png', '포인트 받기', '라이브 쇼핑 보고'),
+  BenefitMenu('$basePath/benefit/benefit_05.png', '추가 혜택 보기', '행운퀴즈 종료'),
+  BenefitMenu('$basePath/benefit/benefit_06.png', '140원 받기', '만보기'),
+];
---
+++ lib/screen/main/tab/benefit/vo_benefit_menu.dart
@@ -0,0 +1,7 @@
+class BenefitMenu {
+  final String imagePath;
+  final String title;
+  final String subTitle;
+
+  BenefitMenu(this.imagePath, this.title, this.subTitle);
+}
