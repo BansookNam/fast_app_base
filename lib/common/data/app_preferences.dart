9017135a6506be4ffdb71354dd93a733b17ec867
Bansook Nam <bs.nam@lawfully.com>
2023-03-22 18:30:16 +0900

    pref key 추가.

--- lib/common/data/app_preferences.dart
+++ lib/common/data/app_preferences.dart
@@ -7,50 +7,57 @@ class AppPreferences {
 
   static late final SharedPreferences _prefs;
 
+  static String getPrefKey(PreferenceItem item) {
+    return '${AppPreferences.prefix}${item.key}';
+  }
+
   static Future<void> init() async {
     _prefs = await SharedPreferences.getInstance();
     return;
   }
 
-  static Future<bool> setValue<T>(PreferenceItem<T> key, T? value) async {
+  static Future<bool> setValue<T>(PreferenceItem<T> item, T? value) async {
+    final String key = getPrefKey(item);
     switch (T) {
       case int:
-        return _prefs.setInt(key.key, value as int);
+        return _prefs.setInt(key, value as int);
       case String:
-        return _prefs.setString(key.key, value as String);
+        return _prefs.setString(key, value as String);
       case double:
-        return _prefs.setDouble(key.key, value as double);
+        return _prefs.setDouble(key, value as double);
       case bool:
-        return _prefs.setBool(key.key, value as bool);
+        return _prefs.setBool(key, value as bool);
       case List<String>:
-        return _prefs.setStringList(key.key, value as List<String>);
+        return _prefs.setStringList(key, value as List<String>);
       default:
         if (value is Enum) {
-          return _prefs.setString(key.key, describeEnum(value));
+          return _prefs.setString(key, describeEnum(value));
         } else {
           throw Exception('$T 타입에 대한 저장 transform 함수를 추가 해주세요.');
         }
     }
   }
 
-  static Future<bool> deleteValue<T>(PreferenceItem<T> key) async {
-    return _prefs.remove(key.key);
+  static Future<bool> deleteValue<T>(PreferenceItem<T> item) async {
+    final String key = getPrefKey(item);
+    return _prefs.remove(key);
   }
 
-  static T? getValue<T>(PreferenceItem<T> key) {
+  static T? getValue<T>(PreferenceItem<T> item) {
+    final String key = getPrefKey(item);
     switch (T) {
       case int:
-        return _prefs.getInt(key.key) as T? ?? key.defaultValue;
+        return _prefs.getInt(key) as T? ?? item.defaultValue;
       case String:
-        return _prefs.getString(key.key) as T? ?? key.defaultValue;
+        return _prefs.getString(key) as T? ?? item.defaultValue;
       case double:
-        return _prefs.getDouble(key.key) as T? ?? key.defaultValue;
+        return _prefs.getDouble(key) as T? ?? item.defaultValue;
       case bool:
-        return _prefs.getBool(key.key) as T? ?? key.defaultValue;
+        return _prefs.getBool(key) as T? ?? item.defaultValue;
       case List<String>:
-        return _prefs.getStringList(key.key) as T? ?? key.defaultValue;
+        return _prefs.getStringList(key) as T? ?? item.defaultValue;
       default:
-        return transform(T, _prefs.getString(key.key)) ?? key.defaultValue;
+        return transform(T, _prefs.getString(key)) ?? item.defaultValue;
     }
   }
 
@@ -67,6 +74,7 @@ class AppPreferences {
     }
   }
 }
+
 
 class PreferenceItem<T> {
   final T? defaultValue;
