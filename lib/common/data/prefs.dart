eae104b41c2ae887045d1d94e96a3146c7160a72
Bansook Nam <bs.nam@lawfully.com>
2023-03-24 17:09:55 +0900

    디폴트 테마를 지정

--- lib/common/data/prefs.dart
+++ lib/common/data/prefs.dart
@@ -2,5 +2,5 @@ import 'package:fast_app_base/common/data/app_preferences.dart';
 import 'package:fast_app_base/common/theme/custom_theme.dart';
 
 class Prefs {
-  static final appTheme = PreferenceItem<CustomTheme>('appTheme');
+  static final appTheme = PreferenceItem<CustomTheme>('appTheme', CustomTheme.light);
 }
