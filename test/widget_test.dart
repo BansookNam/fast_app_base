ff90d288d45901328dea29b94fe613dc250b0d85
Bansook Nam <bs.nam@lawfully.com>
2023-03-24 20:43:18 +0900

    한국어 로케일 설정 체크

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,6 +10,7 @@ import 'dart:io';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:fast_app_base/app.dart';
 import 'package:fast_app_base/common/data/app_preferences.dart';
+import 'package:fast_app_base/common/language/language.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:isar/isar.dart';
@@ -28,13 +29,19 @@ void main() {
   testWidgets('앱 실행 및 기본 텍스트 확인', (WidgetTester tester) async {
     await pumpApp(tester);
     await tester.pumpAndSettle();
+
+    expect(currentLanguage, Language.korean); //startLocale: const Locale('ko') 이 설정되어 있으므로 한국어로 시작
   });
 }
 
 Future<void> pumpApp(WidgetTester tester) async {
   await tester.pumpWidget(EasyLocalization(
-      supportedLocales: const [Locale('en'), Locale('ko')],
-      fallbackLocale: const Locale('en'),
+      supportedLocales: const [
+        Locale('ko'),
+        Locale('en'),
+      ],
+      startLocale: const Locale('ko'),
+      fallbackLocale: const Locale('ko'),
       path: 'assets/translations',
       useOnlyLangCode: true,
       child: const App()));
