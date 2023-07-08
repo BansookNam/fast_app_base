78129c62b748a7264a0a9e02ff596c2eec5f2a3f
Bansook Nam <bs.nam@lawfully.com>
2023-07-08 14:29:55 +0900

    language enum 리팩토링

--- lib/common/language/language.dart
+++ lib/common/language/language.dart
@@ -4,29 +4,16 @@ import '../../app.dart';
 import '../common.dart';
 
 enum Language {
-  korean,
-  english;
+  korean(Locale('ko'), '$basePath/flag/flag_kr.png'),
+  english(Locale('en'), '$basePath/flag/flag_us.png');
+
+  final Locale locale;
+  final String flagPath;
+
+  const Language(this.locale, this.flagPath);
 
   static Language find(String key) {
     return Language.values.asNameMap()[key] ?? Language.english;
-  }
-
-  String get flagPath {
-    switch (this) {
-      case Language.korean:
-        return '$basePath/flag/flag_kr.png';
-      case Language.english:
-        return '$basePath/flag/flag_us.png';
-    }
-  }
-
-  Locale get locale {
-    switch (this) {
-      case Language.korean:
-        return const Locale('ko');
-      case Language.english:
-        return const Locale('en');
-    }
   }
 }
 
