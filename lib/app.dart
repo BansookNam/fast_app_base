70fad96d269abedf1c02f01d02259a5605df4cb2
Bansook Nam <bs.nam@lawfully.com>
2023-09-01 00:22:59 +0900

    테마 찾기가 어렵기 때문에 App으로 변수 이동

--- lib/app.dart
+++ lib/app.dart
@@ -3,8 +3,12 @@ import 'package:fast_app_base/common/theme/custom_theme_app.dart';
 import 'package:fast_app_base/screen/main/s_main.dart';
 import 'package:flutter/material.dart';
 
+import 'common/theme/custom_theme.dart';
+
 class App extends StatefulWidget {
   static final GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+  ///light, dark 테마가 준비되었고, 시스템 테마를 따라가게 하려면 해당 필드를 제거 하시면 됩니다.
+  static const defaultTheme = CustomTheme.light;
   static bool isForeground = true;
 
   const App({super.key});
